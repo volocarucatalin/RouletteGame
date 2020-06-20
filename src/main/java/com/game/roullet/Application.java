4902f9b08fe936b6483b2a9262495aed06388590
Catalin <volocaru.catalin17@gmail.com>
2020-06-20 12:06:59 +0100

    First commit

---
+++ src/main/java/com/game/roullet/Application.java
@@ -0,0 +1,13 @@
+package com.game.roullet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
