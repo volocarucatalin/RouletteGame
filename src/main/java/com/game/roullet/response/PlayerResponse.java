be51b25ed68a43c70d333d5b07c4f24fe38b9667
Catalin <volocaru.catalin17@gmail.com>
2020-07-07 20:27:28 +0100

    Provide Integration test by using the controllers

--- src/main/java/com/game/roullet/response/PlayerResponse.java
+++ src/main/java/com/game/roullet/response/PlayerResponse.java
@@ -1,6 +1,7 @@
 package com.game.roullet.response;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class PlayerResponse implements Serializable {
 
@@ -10,11 +11,28 @@ public class PlayerResponse implements Serializable {
         this.id = id;
     }
 
+    public PlayerResponse() {
+
+    }
+
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PlayerResponse)) return false;
+        PlayerResponse that = (PlayerResponse) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
     }
 }
